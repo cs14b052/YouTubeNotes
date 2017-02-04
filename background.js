cf057620acc8f92637c10e6ced56d5081a02821f
Meghana <sistla.meghana@gmail.com>
2017-02-05 03:57:18 +0530

    icon added

--- background.js
+++ background.js
@@ -2,8 +2,8 @@
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab){
 	
 	if (changeInfo.status == 'complete') {
+		chrome.pageAction.show(tab.id);
     	chrome.tabs.sendMessage(tabID, {url: tab.url}, function(response) {
-      		console.log(response.farewell);
     	});
 	}
 });
