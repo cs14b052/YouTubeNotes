57f1001b6a9da107fbf7a60a73666d72631f6e12
Sowjanya <sowjanyavemuri1997@gmail.com>
2017-02-04 15:15:30 +0530

    adding the basic components for side panel

--- content.js
+++ content.js
@@ -1,20 +1,25 @@
-
 if(document != null){
 	var button = document.createElement("button");
 	button.style.cursor = "pointer";
 	button.appendChild(document.createTextNode("YouTubeNotes"));
 	button.addEventListener('click', function(){
 		var a = addPanel();
+<<<<<<< ab6ff6ab04472e2dc76f206d8f8ec2f933489b8c
+=======
+		
+>>>>>>> adding the basic components for side panel
 	});
 	document.getElementById('watch8-secondary-actions').appendChild(button);
 }	
 function addPanel(){
 	var a = document.getElementById("mainPanel");
-	if(a != null && a.style.display != "none")
+	if(a != null && a.style.display!="none")
 		return a;
 	var videoContainer = document.getElementsByClassName("ytp-iv-video-content")[0];
 	var sidePanel = document.createElement("div");
-	sidePanel.appendChild(document.createElement("p").appendChild(document.createTextNode("dkjshdg")));
+	sidePanel.style.backgroundColor = "rgb(127,8,4)";
+	sidePanel.style.height = videoContainer.clientHeight + "px";
+	// sidePanel.appendChild(document.createElement("p").appendChild(document.createTextNode("dkjshdg")));
 	sidePanel.id = "mainPanel";
 	sidePanel.style.display = "block";
 	var clickFunc = function () {
@@ -41,8 +46,42 @@ function addPanel(){
 				sidePanel.style.display = "block";
 		}
 	});
+
+	var closeBut = document.createElement("button");
+	closeBut.style.cursor = "pointer";
+	closeBut.appendChild(document.createTextNode("close"));
+
+	//styling by Kaaaaaaavs
+
+	sidePanel.appendChild(closeBut);
+	closeBut.addEventListener('click',function(){
+		sidePanel.style.display="none";
+	});
+
+	var addBut = document.createElement("button");
+	addBut.style.cursor = "pointer";
+	addBut.appendChild(document.createTextNode("+ Add Note"));
+
+	//styling by Kaaaaaaavs
+
+	sidePanel.appendChild(addBut);
+	addBut.addEventListener('click',function(){
+		//contact server for this
+	});
+
+	var view = document.createElement("details");
+	view.style.cursor = "pointer";
+	view.appendChild(document.createTextNode("Notes"));
+
+	//styling by Kaaaaaaavs
+
+	sidePanel.appendChild(view);
+	
+	//get content from server
+
 	var sideBar = document.getElementById("watch7-sidebar-contents");
 	sideBar.insertBefore(sidePanel, sideBar.firstChild);
+
 	return sidePanel;
 }
 
