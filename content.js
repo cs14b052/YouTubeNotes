ecff97c2342e38e707997ea8b08c439b9227d7e0
kavya006 <satprathra.1997@gmail.com>
2017-02-04 13:57:23 +0000

    Initial Setup

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
+
+if(document != null){
+	var button = document.createElement("button");
+	button.style.cursor = "pointer";
+	button.appendChild(document.createTextNode("YouTubeNotes"));
+	button.addEventListener('click', function(){
+		var a = addPanel();
+		console.log(a);
+	});
+	document.getElementById('watch8-secondary-actions').appendChild(button);
+}	
+function addPanel(){
+	var videoContainer = document.getElementsByClassName("ytp-iv-video-content")[0];
+	var sidePanel = document.createElement("div");
+	sidePanel.appendChild(document.createElement("p").appendChild(document.createTextNode("dkjshdg")));
+	sidePanel.id = "mainPanel";
+	alert(videoContainer.clientHeight + " " + videoContainer.clientWidth);
+	var theatreMode = document.getElementsByClassName("ytp-size-button");
+	return sidePanel;
+}
+
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	alert('hfgbvrb');
-    alert(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+	function(request, sender, sendResponse){
+		alert('sfkjsd');
+		if(request.greeting == "hello"){
+			sendResponse({farewell : "goodbye!!"});
+		}
+	}
+);
